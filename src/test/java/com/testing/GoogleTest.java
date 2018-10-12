6a05065ad86d5128ff5ba14e3a8a09b5e9d89722
Noman Siddiqi <noman.siddiqi@hotmail.com>
2018-10-12 09:23:05 -0400

    adding extra print ln

--- src/test/java/com/testing/GoogleTest.java
+++ src/test/java/com/testing/GoogleTest.java
@@ -4,6 +4,7 @@
 // pass@1234
 package com.testing;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -15,13 +16,17 @@ import javax.xml.bind.SchemaOutputResolver;
  * Unit test for simple App.
  */
 public class GoogleTest{
-
+    String exp_title = "Google";
     @Test
     public void testGoogle(){
         System.setProperty("webdriver.gecko.driver","C:\\Tools\\August 2018\\geckodriver-v0.22.0-win32\\geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
         driver.get("http://google.com");
-        System.out.println(driver.getTitle());
+        if(exp_title.equalsIgnoreCase(driver.getTitle())){
+            System.out.println("test pass");
+        }else {
+            System.out.println("test fail");
+        }
         driver.quit();
     }
 
