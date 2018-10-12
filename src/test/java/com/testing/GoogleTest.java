fb13dedc4bb1c82b883e0c6f014f4d9384f3107f
Noman Siddiqi <noman.siddiqi@hotmail.com>
2018-10-12 08:48:24 -0400

    Initial commit

---
+++ src/test/java/com/testing/GoogleTest.java
@@ -0,0 +1,26 @@
+//https://svn2.sliksvn.com/sejsoft_sejalbhayana
+
+// sejal
+// pass@1234
+package com.testing;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class GoogleTest{
+
+    @Test
+    public void testGoogle(){
+        System.setProperty("webdriver.gecko.driver","C:\\Tools\\August 2018\\geckodriver-v0.22.0-win32\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://google.com");
+        driver.quit();
+    }
+
+}
